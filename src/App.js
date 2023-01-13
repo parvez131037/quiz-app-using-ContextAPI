74bf1603897980551e2b730910867aff0b152751
parvez131037 <parvezahmed.ansari45@gmail.com>
2023-01-13 20:00:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Result from "./Components/Result";
+import Start from "./Components/Start";
+import Quiz from "./Components/Quiz";
+import { QuizContext } from "./Context/QuizHolder";
+import { useContext } from "react";
 function App() {
+  const { start, exit } = useContext(QuizContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        exit === false
+          ?
+          <>
+            {
+              start === true
+                ?
+                <Quiz />
+                :
+                <Start />
+            }
+          </>
+          : <Result />
+      }
+
+      {/* <Result /> */}
+    </>
   );
 }
 
